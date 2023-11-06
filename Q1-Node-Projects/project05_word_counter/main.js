c6f53912a8ece09d96ca16b81c876374ba12ef7f
Abdul Waris Anjum <warisanjumrox@gmail.com>
2023-11-06 17:46:25 +0500

    Add files via upload

---
+++ Q1-Node-Projects/project05_word_counter/main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+class TextAnalyzer {
+    async getUserInput(message) {
+        const { userInput } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'userInput',
+                message,
+            },
+        ]);
+        return userInput;
+    }
+    countCharacters(text) {
+        // Remove whitespaces and count characters
+        const textWithoutWhitespaces = text.replace(/\s/g, '');
+        return textWithoutWhitespaces.length;
+    }
+    countWords(text) {
+        // Remove whitespaces and count words
+        const wordsArray = text.split(/\s+/).filter(word => word.length > 0);
+        return wordsArray.length;
+    }
+    async start() {
+        console.log('Welcome to the Text Analyzer!\n');
+        const paragraph = await this.getUserInput('Enter an English paragraph:');
+        const characterCount = this.countCharacters(paragraph);
+        const wordCount = this.countWords(paragraph);
+        console.log(`\nCharacter count (excluding whitespaces): ${characterCount}`);
+        console.log(`Word count (excluding whitespaces): ${wordCount}\n`);
+    }
+}
+const textAnalyzer = new TextAnalyzer();
+textAnalyzer.start();
